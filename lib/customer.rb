88fd4aba08eb37d47ce54cfb8f95eb66cb70eb71
github username <github email address>
2019-09-20 09:13:17 +0100

    completed all classes

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,35 @@
+require "pry"
+
 class Customer
-  
+
+    attr_reader :name, :age
+
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        Meal.all.select {|meal| meal.customer == self}
+    end
+
+    def waiters
+        waiter_array = []
+        meals.select {|meal| waiter_array << meal.waiter}
+        return waiter_array
+    end
+
+
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,45 @@
+require "pry"
+
 class Waiter
+
+    attr_reader :name, :years_of_experience
+
+    @@all = []
+
+    def initialize(name, experience)
+        @name = name
+        @ayears_of_experience = experience
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select {|meal| meal.waiter == self}
+    end
+
+    def best_tipper
+        big_tip = 0
+        big_tipper = ""
+        meals.select {|meal| 
+            if meal.tip > big_tip 
+                big_tip = meal.tip
+                big_tipper = meal.customer
+            end
+        }
+        return big_tipper
+    end
+
+
+
+
+
+
 
 end
